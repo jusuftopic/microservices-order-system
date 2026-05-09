73d9a7e61701d5d7115c4761c902b677243d022d
TOPIC Jusuf <Jusuf.TOPIC@frequentis.com>
2026-05-09 12:46:36 +0200

    [ORDER-SERVICE] Add artificial test write to Kafka

--- order-service/src/main/java/org/example/orderservice/service/publisher/OutboxEventPublisherService.java
+++ order-service/src/main/java/org/example/orderservice/service/publisher/OutboxEventPublisherService.java
@@ -1,8 +1,11 @@
 package org.example.orderservice.service.publisher;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.commons.event.EventConstants;
+import org.example.commons.event.PaymentRequestedEvent;
 import org.example.orderservice.entity.OutboxDlqEvent;
 import org.example.orderservice.entity.OutboxEvent;
 import org.example.orderservice.repository.OutboxDlqRepository;
@@ -12,8 +15,10 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Service responsible for publishing Outbox Events to Kafka.
@@ -36,6 +41,9 @@ public class OutboxEventPublisherService {
     private final OutboxDlqRepository dlqRepository;
     private final KafkaTemplate<String, String> kafkaTemplate;
 
+    private final ObjectMapper objectMapper;
+    private boolean isSet = false;
+
     /**
      * Publishes all pending outbox events.
      *
@@ -49,7 +57,35 @@ public class OutboxEventPublisherService {
 
         List<OutboxEvent> events = outboxRepository.findByProcessedFalseOrderByCreatedAtAsc();
         if (events == null || events.isEmpty()) {
-            log.info("[ORDER-SERVICE][OUTBOX-PUBLISHER] No pending events found.");
+            log.debug("[ORDER-SERVICE][OUTBOX-PUBLISHER] No pending events found.");
+            if (!isSet) {
+                final OutboxEvent event = new OutboxEvent();
+                event.setId(UUID.randomUUID());
+                event.setAggregateType("TEST");
+                event.setAggregateId(1L);
+                event.setEventType("TEST");
+                try {
+                    event.setPayload(
+                            objectMapper.writeValueAsString(
+                                    new PaymentRequestedEvent(
+                                            UUID.randomUUID(),
+                                            1L, BigDecimal.valueOf(1L), "test"
+                                    )
+                            )
+                    );
+                }
+                catch (Exception e) {
+                    event.setPayload("");
+                }
+
+                event.setProcessed(true);
+                event.setRetryCount(0);
+
+                publishSingleEvent(event);
+                isSet = true;
+            }
+
+
             return;
         }
 
