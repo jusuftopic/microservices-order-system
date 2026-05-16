40f335c6a5b87e3bf0870237b73133d30799a45f
TOPIC Jusuf <Jusuf.TOPIC@frequentis.com>
2026-05-16 13:21:26 +0200

    [INVENTORY-SERVICE] Add InventoryService

---
+++ inventory-service/src/main/java/org/example/inventoryservice/service/InventoryService.java
@@ -0,0 +1,14 @@
+package org.example.inventoryservice.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service layer responsible for handling business logic related to Order Inventory.
+ */
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class InventoryService {
+}
