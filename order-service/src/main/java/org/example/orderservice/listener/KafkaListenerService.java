e7adddada12ccd355e4e82fd94f6b3eb12c257a6
TOPIC Jusuf <Jusuf.TOPIC@frequentis.com>
2026-06-05 16:39:56 +0200

    [ORDER-SERVICE] Modify Kafka Listener on class level

--- order-service/src/main/java/org/example/orderservice/listener/KafkaListenerService.java
+++ order-service/src/main/java/org/example/orderservice/listener/KafkaListenerService.java
@@ -7,6 +7,7 @@ import org.example.commons.event.contracts.InventoryFailedEvent;
 import org.example.commons.event.contracts.InventoryReservedEvent;
 import org.example.orderservice.service.OrderService;
 import org.example.orderservice.utils.Constants;
+import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -23,38 +24,48 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@KafkaListener(
+        topics = EventConstants.TOPIC_ORDER_INVENTORY_RESPONSE_V1,
+        groupId = Constants.KAFKA_ORDER_GROUP_ID
+)
 public class KafkaListenerService {
 
     private final OrderService orderService;
 
     /**
-     * Handles event response from Inventory Service
+     * Handles successfully event response from Inventory Service
      *
-     * @param event Event indicates successfully or failed item reservation
+     * @param reserved Event indicates successfully item reservation
      */
-    @KafkaListener(
-            topics = EventConstants.TOPIC_ORDER_INVENTORY_RESPONSE_V1,
-            groupId = Constants.KAFKA_ORDER_GROUP_ID
-    )
-    public void handleInventoryResponse(Object event) {
-        log.info("[ORDER-SERVICE][KAFKA] Received event type {}",
-                event.getClass().getSimpleName());
+    @KafkaHandler
+    public void handleInventoryReserved(InventoryReservedEvent reserved) {
+        log.info("[ORDER-SERVICE][KAFKA] Received InventoryReservedEvent for order {} correlationId {}",
+                reserved.orderId(), reserved.correlationId());
 
-        if (event instanceof InventoryReservedEvent reserved) {
-            log.info("[ORDER-SERVICE][KAFKA] Inventory RESERVED for order {} correlationId {}",
-                    reserved.orderId(), reserved.correlationId());
-
-            orderService.handleInventoryReserved(reserved);
-
-        } else if (event instanceof InventoryFailedEvent failed) {
-            log.warn("[ORDER-SERVICE][KAFKA] Inventory FAILED for order {} reason {} correlationId {}",
-                    failed.orderId(), failed.reason(), failed.correlationId());
-
-            orderService.handleInventoryFailed(failed);
-
-        } else {
-            log.warn("[ORDER-SERVICE][KAFKA] Unknown event type received: {}", event);
-        }
+        orderService.handleInventoryReserved(reserved);
     }
 
+    /**
+     * Handles failed event response from Inventory Service
+     *
+     * @param failed Event indicates failed item reservation
+     */
+    @KafkaHandler
+    public void handleInventoryFailed(InventoryFailedEvent failed) {
+        log.info("[ORDER-SERVICE][KAFKA] Received InventoryFailedEvent for order {}. Reason: {}",
+                failed.orderId(), failed.reason());
+
+        orderService.handleInventoryFailed(failed);
+    }
+
+    /**
+     * Catch-all fallback method to capture unmapped data shapes safely without breaking consumers
+     *
+     * @param unknownMessage Unknown message
+     */
+    @KafkaHandler(isDefault = true)
+    public void handleUnknownObject(Object unknownMessage) {
+        log.error("[ORDER-SERVICE][KAFKA] Unmatched event signature fallback triggered! Payload object type: {}",
+                unknownMessage.getClass().getName());
+    }
 }
