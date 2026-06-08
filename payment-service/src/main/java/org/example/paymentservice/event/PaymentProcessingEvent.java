79e416b78dde0695fb848e73f2132cadb6fba663
TOPIC Jusuf <Jusuf.TOPIC@frequentis.com>
2026-06-08 19:42:41 +0200

    [PAYMENT] Trigger pipeline

--- payment-service/src/main/java/org/example/paymentservice/event/PaymentProcessingEvent.java
+++ payment-service/src/main/java/org/example/paymentservice/event/PaymentProcessingEvent.java
@@ -4,7 +4,6 @@ package org.example.paymentservice.event;
  * Internal event representing that a payment is ready to be processed
  * by an external payment provider.
  *
- * IMPORTANT: This is NOT persisted — it is only used for async workflow.
  */
 public record PaymentProcessingEvent(
         Long paymentId,
