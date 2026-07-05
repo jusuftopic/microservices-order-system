1ac189c8eed116ad7b02f657b2d46c54b9c63eca
TOPIC Jusuf <Jusuf.TOPIC@frequentis.com>
2026-07-05 15:32:50 +0200

    [JENKINS] Trigger on messaging-starter changes

--- messaging-starter/src/test/java/org/example/messagingstarter/outbox/service/OutboxEventPublisherServiceTest.java
+++ messaging-starter/src/test/java/org/example/messagingstarter/outbox/service/OutboxEventPublisherServiceTest.java
@@ -51,7 +51,7 @@ public class OutboxEventPublisherServiceTest {
         service.publishPendingEvents();
 
         // THEN
-        verifyNoInteractions();
+        verifyNoInteractions(eventPublisherService);
         verify(outboxRepository).findReadyForPublishing(any());
     }
 
